64722cf3a74b37280398afa3278e43cb8bfb194d
mahdi bahrami <mahdibahrami@mahdis-MacBook-Pro.local>
2024-11-27 22:55:31 +0300

    m

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
-import basicSsl from '@vitejs/plugin-basic-ssl';
+//import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +13,7 @@ export default defineConfig({
     react(),
     // Allows using self-signed certificates to run the dev server using HTTPS.
     // https://www.npmjs.com/package/@vitejs/plugin-basic-ssl
-    basicSsl(),
+    //basicSsl(),
   ],
   publicDir: './public',
   server: {
