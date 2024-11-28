a02a06e4f60dcf04c32155d6defa7358d562e12e
mahdi bahrami <mahdibahrami@mahdis-MacBook-Pro.local>
2024-11-28 16:06:47 +0300

    m

--- src/navigation/routes.jsx
+++ src/navigation/routes.jsx
@@ -1,6 +1,7 @@
 import Home from '@/pages/Home';
 import RegisterPage from '@/pages/Register'; // Fixed typo
 
+
 export const routes = [
   { path: '/', Component: Home },
   { path: '/register', Component: RegisterPage }
